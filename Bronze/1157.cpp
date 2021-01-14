65b452fb69e8c4a8fdf2c33328d5f41b7e12b9b6
hwRG <hws0120@khu.ac.kr>
2021-01-14 21:00:33 +0900

    BAEKJOON Start

---
+++ Bronze/1157.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <cmath>
+#include <string>
+using namespace std;
+
+int main() {
+	int wordList[26][2], top, spot = -1;
+	string word;
+
+	for (int i = 0; i < 26; i++) {
+		wordList[i][0] = i + 65;
+		wordList[i][1] = 0;
+	}
+
+	cin >> word;
+	for(int i = 0; i < word.length(); i++){
+		if (word[i] >= 97 && word[i] <= 122) 
+			word[i] = word[i] - 32;
+		for (int j = 0; j < 26; j++) 
+			if (wordList[j][0] == word[i]) 
+				wordList[j][1]++;
+	}
+	top = wordList[0][1];
+	for (int i = 0; i < 26; i++) {
+		if (top < wordList[i][1]){
+			top = wordList[i][1];
+			spot = i;
+		}
+	}
+	if (spot == -1) 
+		spot = 0;
+
+	for (int i = 0; i < 26; i++){
+		if (top == wordList[i][1]){
+			if (i == spot)
+				continue;
+			cout << '?' << endl;
+			return 0;
+		}
+	}
+	cout << (char)wordList[spot][0] << endl;
+
+	return 0;
+}

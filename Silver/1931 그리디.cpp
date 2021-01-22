9d5a3fd962174f5006aa3d71d00f49f77dbf978a
hanwoo <55057972+hwRG@users.noreply.github.com>
2021-01-22 11:50:17 +0900

    Add files via upload

---
+++ Silver/1931 그리디.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int N;
+	int* start = new int[N];
+	int* end = new int[N];
+	cin >> N;
+
+	for (int i = 0; i < N; i++) {
+		cin >> start[i] >> end[i];
+
+	}
+
+}

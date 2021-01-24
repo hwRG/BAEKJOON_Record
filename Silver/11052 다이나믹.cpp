77a9b5d9c6fa73894ef4caa6a2f0fde84d08ce63
hwRG <hws0120@khu.ac.kr>
2021-01-25 01:54:24 +0900

    Greedy

--- Silver/11052 다이나믹.cpp
+++ Silver/11052 다이나믹.cpp
@@ -1,15 +1,18 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
+int dp[1001];
+int p[10001];
+
 int main() {
-
-	int N, price;
+	int N;
 	cin >> N;
-	int card[10000][2];
+	for (int i = 1; i <= N; i++) 
+		cin >> p[i];
 
-	for (int i = 0; i < N; i++) {
-		cin >> price;
-		card[i][0] = i + 1;
-		card[i][1] = price;
-	}
+	for (int i = 1; i <= N; i++)
+		for (int j = 1; j <= i; j++)
+			if (dp[i] < dp[i - j] + p[j]) dp[i] = dp[i - j] + p[j];
+	cout << dp[N];
 }

a3cd2580fc8b7ee543b82a40ac5587e34e1783dd
Hanwoo <55057972+hwRG@users.noreply.github.com>
2021-02-11 23:51:28 +0900

    Add files via upload

---
+++ Gold/2357 자료구조.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <list>
+using namespace std;
+
+int arr[100001];
+
+int main() {
+	ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+	int N, M, a, b;
+	cin >> N >> M;
+	for (int i = 1; i <= N; i++) 
+		cin >> arr[i];
+
+	for (int i = 1; i <= M; i++) {
+		cin >> a >> b;
+
+	}
+
+
+}

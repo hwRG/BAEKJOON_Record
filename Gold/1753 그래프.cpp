ed72efa9b7812e8331b26dddd4ad1668c36bbc32
hwRG <hws0120@khu.ac.kr>
2021-02-18 23:40:37 +0900

    Graph Progress

---
+++ Gold/1753 그래프.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+using namespace std;
+
+struct Linkedlist
+{
+	int value = 0;
+	int line = 0;
+	Linkedlist* next = NULL;
+};
+
+Linkedlist** edge = new Linkedlist*[20001];
+int V, E, K, u, v, w;
+
+void link() {
+	Linkedlist* location;
+
+	location = new Linkedlist;
+	location->value = v;
+	location->line = w;
+	location->next = edge[u];
+	edge[u] = location;
+}
+
+int main() {
+	ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+	cin >> V >> E >> K;
+	for (int k = 0; k < E; k++) {
+		cin >> u >> v >> w;
+		link();
+	}
+}
+
+/*
+void link2() {
+	Linkedlist* newNode;
+	Linkedlist* predLoc;
+	Linkedlist* location;
+	bool found;
+
+	newNode = new Linkedlist;
+	newNode->value = v;
+	newNode->line = w;
+	newNode->next = NULL;
+	if (edge[u].value != 0) {
+		newNode->next = predLoc->next;
+		predLoc->next = newNode;
+	}
+	else {
+		edge[u] = *newNode;
+		newNode->next = newNode;
+	}
+}
+void link1() {
+	Linkedlist count = edge[u];
+	Linkedlist temp;
+	while (count.next != NULL)
+		count = *count.next;
+	temp.value = w;
+	temp.line = v;
+	temp.next = NULL;
+	count.next = &temp;
+}*/

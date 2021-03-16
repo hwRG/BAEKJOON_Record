512a82385eef40963cfd6fae7c49e0604a2a6852
hwRG <hws0120@khu.ac.kr>
2021-03-17 00:38:50 +0900

    Graph

--- Silver/2644 그래프.cpp
+++ Silver/2644 그래프.cpp
@@ -5,19 +5,43 @@ using namespace std;
 
 int n, st, en, line, a, b;
 vector<int> edge[101];
-bool visited[101];
+bool visited[101], flag;
 
 void BFS() {
-	int count = 0;
+	int count = 1;
 	queue<int> que;
 	que.push(st);
 	int cur = que.front();
 	visited[cur] = true;
 	que.pop();
 	for (int i = 0; i < edge[cur].size(); i++) {
-		que.push();
+		que.push(edge[cur][i]);
+		visited[edge[cur][i]] = true;
 	}
-	
+	while (1) {
+		if (que.empty()){
+			cout << -1;
+			break;
+		}
+		int length = que.size();
+		for (int i = 0; i < length; i++) {
+			cur = que.front();
+			if (cur == en) {
+				flag = true;
+				cout << count;
+				break;
+			}
+			que.pop();
+			for (int j = 0; j < edge[cur].size(); j++) {
+				if (!visited[edge[cur][j]]) {
+					que.push(edge[cur][j]);
+					visited[edge[cur][j]] = true;
+				}
+			}
+		}
+		if (flag) break;
+		count++;
+	}
 }
 
 int main() {

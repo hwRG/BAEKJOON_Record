fd66e8a591566bc847adf002f386ae5b522d290c
hwRG <hws0120@khu.ac.kr>
2021-05-14 20:03:34 +0900

    Programmers

--- Programmers/프로그래머스 그리디1.cpp
+++ Programmers/프로그래머스 그리디1.cpp
@@ -1,8 +1,39 @@
 #include <iostream>
+#include <vector>
 using namespace std;
+
 
 int main() {
 	ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+	int n = 5;
+	bool ok[31];
+	vector<int> lost = { 3,2,4 };
+	vector<int> reserve = { 1, 3, 5 };
+	int answer = 0;
 
+	for (int i = 0; i < lost.size(); i++) {
+		for (int j = 0; j < reserve.size(); j++) {
+			if (lost[i] == reserve[j]) {
+				lost.erase(lost.begin() + i);
+				reserve.erase(reserve.begin() + j);
+			}
+		}
+	}
+	answer = n - lost.size();
 
+	for (auto i : lost) {
+		for (auto j : reserve) {
+			if (i - 1 == j && ok[j]) {
+				answer++;
+				ok[j] = false;
+				break;
+			}
+			else if (i + 1 == j && ok[j]) {
+				answer++;
+				ok[j] = false;
+				break;
+			}
+		}
+	}
+	cout << answer;
 }
--- Programmers/프로그래머스 정렬2.cpp
+++ Programmers/프로그래머스 정렬2.cpp
@@ -1,15 +1,35 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <algorithm>
 using namespace std;
+
+bool check[10];
 
 int main() {
 	ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
-	vector<int> numbers = { 6, 10, 2 };
+	vector<int> numbers = { 0,0,0,0 };
 	string answer = "";
+	vector<pair<string, string>> list;
+	int sum = 0;
+	for(auto i : numbers)
+		sum += i;
+	if (sum == 0) return 0;
+
+	for (int i : numbers) 
+		list.push_back({ to_string(i), to_string(i) });
+	for (int i = 0; i < list.size(); i++) {
+		int j = 0;
+		while(list[i].first.size() != 6){
+			list[i].first += list[i].second[j % list[i].second.size()];
+			j++;
+		}
+	}
 	
-
-
+	sort(list.begin(), list.end());
+	
+	for (int i = list.size() - 1; i > -1; i--)
+		answer += list[i].second;
 
 	cout << answer;
 }
